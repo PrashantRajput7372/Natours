58fd02605eb6e3ad974509e063ac633e08e6a5a9
PrashantRajput7372 <prashantsingh113113@gmail.com>
2025-06-08 20:21:04 +0530

    Added env variable in cookies option

--- Controler/authController.js
+++ Controler/authController.js
@@ -20,7 +20,9 @@ const createSendToken = (user, statuscode, res, message, showUser = true) => {
   const token = signToken(user._id);
 
   const cookieOption = {
-    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
 
     httpOnly: true,
   };
